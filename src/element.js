e3c75197bf8c0585f1eed5cef1a7559e31daeec6
Tyler <nguyentyler97@gmail.com>
2022-12-06 21:25:31 -0800

    feat: add tab setup function WIP

--- src/element.js
+++ src/element.js
@@ -1,11 +1,5 @@
 export const create = (type, selector, selectorName, text, appendParent) => {
     let element = document.createElement(type)
-    // if (selector == 'class') {
-    //     if (selectorName.indexOf(' ') >= 0) {
-
-    //     }
-    //     element.classList.add(selectorName)
-    // }
     if (selector == 'class') {
         element.classList.add(selectorName)
     }
--- src/index.js
+++ src/index.js
@@ -7,17 +7,36 @@ import './style.css'
     const tabContainer = ele.create('div', 'class', 'tabContainer', '', mainContent)
     const tabHome = ele.create('button', 'class', 'tab', 'Home', tabContainer)
     tabHome.classList.add('home')
+    tabHome.setAttribute('data-for-tab', '1')
     const tabMenu = ele.create('button', 'class', 'tab', 'Menu', tabContainer)
     tabMenu.classList.add('menu')
+    tabMenu.setAttribute('data-for-tab', '2')
     const tabContact = ele.create('button', 'class', 'tab', 'Contact', tabContainer)
     tabContact.classList.add('contact')
+    tabContact.setAttribute('data-for-tab', '3')
 
-    const tabContentContainer = ele.create('div', 'class', 'tabContentContainer', '', mainContent)
-    const tabContentHome = ele.create('div', 'class', 'tab', 'Home Content', tabContentContainer)
-    tabContentHome.classList.add('content', 'home')
-    const tabContentMenu = ele.create('div', 'class', 'tab', 'Menu Content', tabContentContainer)
-    tabContentMenu.classList.add('content', 'menu')
-    const tabContentContact = ele.create('div', 'class', 'tab', 'Contact Content', tabContentContainer)
-    tabContentContact.classList.add('content', 'contact')
+    const tabContentHome = ele.create('div', 'class', 'content', 'Home: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse auctor venenatis libero. Vivamus viverra in metus a maximus. Aliquam a libero ut erat aliquam ultricies. Nullam quis mi libero. Curabitur tempor faucibus dui. Suspendisse nec orci maximus, interdum velit in, molestie ligula. Fusce ornare nisl metus, sed facilisis felis mattis eu. Proin felis massa, efficitur sit amet magna ac, consectetur iaculis libero. In ut sapien a lorem sollicitudin mattis in non erat. Ut placerat faucibus nisl, sed cursus turpis sodales at. Maecenas vestibulum vulputate consequat. Proin enim nisl, commodo eget condimentum ut, dictum ac felis. Nam id diam non sem rutrum molestie. Nunc turpis eros, iaculis ac nunc ut, venenatis hendrerit mi.', mainContent)
+    tabContentHome.classList.add('home')
+    tabContentHome.setAttribute('data-tab', '1')
+    const tabContentMenu = ele.create('div', 'class', 'content', 'Menu: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus a augue ut elit vulputate commodo a eget risus. Fusce iaculis malesuada nisi, eget condimentum urna luctus ut. Nam vel tincidunt nunc, fringilla sodales massa. Nunc bibendum neque dolor, et auctor neque congue id. Duis non nunc felis. Donec odio ipsum, volutpat eu enim sit amet, varius pellentesque lacus. Aliquam auctor nec arcu id tincidunt. Donec sapien tellus, faucibus placerat congue quis, luctus vel velit. Aliquam et tortor faucibus, pulvinar dolor eu, pretium ante.', mainContent)
+    tabContentMenu.classList.add('menu')
+    tabContentMenu.setAttribute('data-tab', '2')
+    const tabContentContact = ele.create('div', 'class', 'content', 'Contact: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla at nibh tortor. Vivamus quis lobortis dui. Curabitur ut lectus mi. Etiam consequat et elit non hendrerit. Sed elementum pulvinar eros, sit amet luctus sem feugiat id. Nam condimentum euismod eros, in tincidunt leo interdum non. Aliquam mattis rhoncus mattis. Nullam diam nulla, lacinia eget ultricies vel, euismod ut augue. Fusce ac placerat augue. Integer finibus nisl quis libero mollis, fermentum mollis arcu pretium. Phasellus vel nibh in neque dignissim pharetra vitae at lorem. Aliquam dignissim posuere placerat. Duis vulputate nisi vitae mi dignissim sollicitudin. In congue, mi at dignissim rhoncus, ante enim vestibulum magna, ac iaculis massa lacus vitae lorem. Ut nec eros vel est tempor cursus. Aenean ultrices interdum pulvinar.', mainContent)
+    tabContentContact.classList.add('contact')
+    tabContentContact.setAttribute('data-tab', '3')
 
+    const setupTabs = () => {
+        document.querySelectorAll('.tab').forEach(button => {
+            button.addEventListener('click', () => {
+                const tabNumber = button.dataset.forTab
+                const tabToActivate = mainContent.querySelector(`.content[data-tab='${tabNumber}']`)
+                console.log(tabNumber)
+                console.log(tabToActivate)
+            })
+        })
+    }
+
+    document.addEventListener('DOMContentLoaded', () => {
+        setupTabs()
+    })
 })()
