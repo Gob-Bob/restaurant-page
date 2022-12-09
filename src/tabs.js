c91ef1fabde36a36cd5ba9285b69a7ab4d3e8bdd
Tyler <nguyentyler97@gmail.com>
2022-12-09 13:54:10 -0800

    feat: add basic html for open hours

--- src/tabs.js
+++ src/tabs.js
@@ -16,17 +16,24 @@ const homeTab = (() => {
 
     const customerReview = (() => {
         const container = elementCreation.create('div', 'class', 'review-container', '', tabContentContainer)
-        const review = elementCreation.create('div', 'class', 'review', "I had a truly delightful dining experience at this restaurant, thanks to the fantastic food and top-notch service. The atmosphere was cozy and inviting, and I felt right at home. I would highly recommend this restaurant to anyone, and I'll definitely be coming back! Thank you for a memorable evening.", container)
-        const customer = elementCreation.create('div', 'class', 'customer', ' - Jane Doe', container)
+        elementCreation.create('div', 'class', 'review', "I had a truly delightful dining experience at this restaurant, thanks to the fantastic food and top-notch service. The atmosphere was cozy and inviting, and I felt right at home. I would highly recommend this restaurant to anyone, and I'll definitely be coming back! Thank you for a memorable evening.", container)
+        elementCreation.create('div', 'class', 'customer', ' - Jane Doe', container)
     })()
 
     const restaurantHours = (() => {
-        const title = elementCreation.create('div', 'class', 'hours-title', 'Hours', tabContentContainer)
-    })
+        const mainContainer = elementCreation.create('div', 'class', 'hours-container', '', tabContentContainer)
+        elementCreation.create('div', 'class', 'hours-title', '', mainContainer)
+        const hoursContainer = elementCreation.create('div', 'class', 'hours-container', '', mainContainer)
+        const daysArray = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+        const hoursOfOpening = ': 9am - 10pm'
+        daysArray.forEach(day => {
+            elementCreation.create('div', 'class', 'days-time-open', day + hoursOfOpening, hoursContainer)
+        })
+    })()
 
     const variablesNeedingClass = [
         tabButton,
-        tabContent,
+        tabContentContainer,
         tabTitle
     ]
 
