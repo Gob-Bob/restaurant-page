7decd6ddfebbeadd3ee6cf4d675e5b5b4b225f4f
Tyler <nguyentyler97@gmail.com>
2022-12-13 19:19:49 -0800

    fix: correctly display and hide home tab

--- src/index.js
+++ src/index.js
@@ -6,37 +6,9 @@ import './style.css'
     const mainContent = document.getElementById('content')
 
     tabs.homeTab()
-
-    const tabMenu = elementCreation.create('button', 'class', 'tab', 'Menu', tabs.tabContainer)
-    tabMenu.classList.add('menu')
-    tabMenu.setAttribute('data-for-tab', '2')
-    const tabContact = elementCreation.create('button', 'class', 'tab', 'Contact', tabs.tabContainer)
-    tabContact.classList.add('contact')
-    tabContact.setAttribute('data-for-tab', '3')
-
-    const tabContentMenu = elementCreation.create('div', 'class', 'content', 'Menu: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus a augue ut elit vulputate commodo a eget risus. Fusce iaculis malesuada nisi, eget condimentum urna luctus ut. Nam vel tincidunt nunc, fringilla sodales massa. Nunc bibendum neque dolor, et auctor neque congue id. Duis non nunc felis. Donec odio ipsum, volutpat eu enim sit amet, varius pellentesque lacus. Aliquam auctor nec arcu id tincidunt. Donec sapien tellus, faucibus placerat congue quis, luctus vel velit. Aliquam et tortor faucibus, pulvinar dolor eu, pretium ante.', mainContent)
-    tabContentMenu.classList.add('menu')
-    tabContentMenu.setAttribute('data-tab', '2')
-    const tabContentContact = elementCreation.create('div', 'class', 'content', 'Contact: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla at nibh tortor. Vivamus quis lobortis dui. Curabitur ut lectus mi. Etiam consequat et elit non hendrerit. Sed elementum pulvinar eros, sit amet luctus sem feugiat id. Nam condimentum euismod eros, in tincidunt leo interdum non. Aliquam mattis rhoncus mattis. Nullam diam nulla, lacinia eget ultricies vel, euismod ut augue. Fusce ac placerat augue. Integer finibus nisl quis libero mollis, fermentum mollis arcu pretium. Phasellus vel nibh in neque dignissim pharetra vitae at lorem. Aliquam dignissim posuere placerat. Duis vulputate nisi vitae mi dignissim sollicitudin. In congue, mi at dignissim rhoncus, ante enim vestibulum magna, ac iaculis massa lacus vitae lorem. Ut nec eros vel est tempor cursus. Aenean ultrices interdum pulvinar.', mainContent)
-    tabContentContact.classList.add('contact')
-    tabContentContact.setAttribute('data-tab', '3')
-
-    const setupTabs = () => {
-        document.querySelectorAll('.tab').forEach(button => {
-            button.addEventListener('click', () => {
-                const tabNumber = button.dataset.forTab
-                const tabToActivate = mainContent.querySelector(`.content[data-tab='${tabNumber}']`)
-                tabs.tabContainer.querySelectorAll('.tab').forEach(button => {
-                    button.classList.remove('tab-active')
-                })
-                mainContent.querySelectorAll('.content').forEach(content => {
-                    content.classList.remove('content-active')
-                })
-                button.classList.add('tab-active')
-                tabToActivate.classList.add('content-active')
-            })
-        })
-    }
+    tabs.menuTab()
+    tabs.contactTab()
+    tabs.setupTabs()
 
     const footer = (() => {
         const container = elementCreation.create('div', 'class', 'footer-container', '', mainContent)
@@ -45,8 +17,8 @@ import './style.css'
         const help = elementCreation.create('div', 'class', 'footer-help', 'Need help? Visit our support page for assistance', container)        
     })()
 
-    document.addEventListener('DOMContentLoaded', () => {
-        setupTabs()
-        tabs.tabContainer.querySelector('.tab').click()
-    })
+    // document.addEventListener('DOMContentLoaded', () => {
+    //     tabs.setupTabs()
+    //     tabs.tabContainer.querySelector('.tab').click()
+    // })
 })()
--- src/tabs.js
+++ src/tabs.js
@@ -1,16 +1,14 @@
 import * as elementCreation from './element'
 
 const mainContent = document.getElementById('content')
-
 export const tabContainer = elementCreation.create('div', 'class', 'tabContainer', '', mainContent)
 
 export const homeTab = (() => {
     const tabButton = elementCreation.create('button', 'class', 'tab', 'Home', tabContainer)
-    tabButton.setAttribute('data-for-tab', '1')
-
+    tabButton.classList.add('tab-active')
     const tabContentContainer = elementCreation.create('div', 'class', 'content', '', mainContent)
-    tabContentContainer.classList.add('home-content-container')
-    tabContentContainer.setAttribute('data-tab', '1')
+    tabContentContainer.classList.add('home-main-container')
+    tabContentContainer.classList.add('content-active')
 
     const tabTitle = elementCreation.create('div', 'class', 'home-title', 'Welcome to the Scrap Yard!', tabContentContainer)
 
@@ -30,9 +28,55 @@ export const homeTab = (() => {
             elementCreation.create('div', 'class', 'days-time-open', day + hoursOfOpening, hoursContainer)
         })
     })()
+
     const restaurantLocation = (() => {
         const mainContainer = elementCreation.create('div', 'class', 'location-container', '', tabContentContainer)
         elementCreation.create('div', 'class', 'location-title', 'Location', mainContainer)
         elementCreation.create('div', 'class', 'address', '123 Acme Drive, Corporation, CA', mainContainer)
     })()
 })
+
+export const menuTab = (() => {
+    const tabButton = elementCreation.create('button', 'class', 'tab', 'Menu', tabContainer)
+    const tabContentContainer = elementCreation.create('div', 'class', 'content', 'Menu: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec quis erat sed elit scelerisque cursus. Fusce dignissim volutpat justo, in pretium lectus viverra a. Suspendisse potenti. Morbi vitae enim accumsan, euismod tellus at, facilisis tortor. Duis rutrum turpis sed velit volutpat, sed pellentesque quam euismod. Proin quis erat quis erat auctor fermentum at quis orci. Nam dignissim leo at urna bibendum, quis tincidunt arcu lobortis. In hac habitasse platea dictumst. Sed in nunc quis nunc imperdiet aliquet.', mainContent)
+})
+
+export const contactTab = (() => {
+    const tabButton = elementCreation.create('button', 'class', 'tab', 'Contact', tabContainer)
+    const tabContentContainer = elementCreation.create('div', 'class', 'content', 'Contact: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec quis erat sed elit scelerisque cursus. Fusce dignissim volutpat justo, in pretium lectus viverra a. Suspendisse potenti. Morbi vitae enim accumsan, euismod tellus at, facilisis tortor. Duis rutrum turpis sed velit volutpat, sed pellentesque quam euismod. Proin quis erat quis erat auctor fermentum at quis orci. Nam dignissim leo at urna bibendum, quis tincidunt arcu lobortis. In hac habitasse platea dictumst. Sed in nunc quis nunc imperdiet aliquet.', mainContent)
+})
+
+export const setupTabs = () => {
+    const allTabs = document.querySelectorAll('.tab'),
+        allTabContent = document.querySelectorAll('.content')
+
+    allTabs.forEach((tab, index) => {
+        tab.addEventListener('click', () => {
+            allTabContent.forEach(content => {
+                content.classList.remove('content-active')
+            })
+            allTabs.forEach(tab => {
+                tab.classList.remove('tab-active')
+            })
+            allTabs[index].classList.add('tab-active')
+            allTabContent[index].classList.add('content-active')
+        })
+    })
+}
+
+// export const setupTabs = () => {
+//     document.querySelectorAll('.tab').forEach(button => {
+//         button.addEventListener('click', () => {
+//             const tabNumber = button.dataset.forTab
+//             const tabToActivate = mainContent.querySelector(`.content[data-tab='${tabNumber}']`)
+//             tabs.tabContainer.querySelectorAll('.tab').forEach(button => {
+//                 button.classList.remove('tab-active')
+//             })
+//             mainContent.querySelectorAll('.content').forEach(content => {
+//                 content.classList.remove('content-active')
+//             })
+//             button.classList.add('tab-active')
+//             tabToActivate.classList.add('content-active')
+//         })
+//     })
+// }
