52a7741fa297df08541c45ddd6699fecaf72504f
Tyler <nguyentyler97@gmail.com>
2022-12-15 11:39:26 -0800

    feat: add entree menu items

--- src/tabs.js
+++ src/tabs.js
@@ -3,6 +3,11 @@ import mobilImageFile from './mobil.jpg'
 import superTechImageFile from './supertech.jpg'
 import nutsBoltsImageFile from './nuts&bolts.jpg'
 import spareTireImageFile from './spareTire.jpg'
+import gasolineImageFile from './gasoline.jpeg'
+import scrapMetalImageFile from './scrapmetal.png'
+import carBatteryImageFile from './carbattery.png'
+import cablesImageFile from './cables.png'
+
 
 const mainContent = document.getElementById('content')
 export const tabContainer = elementCreation.create('div', 'class', 'tabContainer', '', mainContent)
@@ -40,11 +45,17 @@ export const menuTab = () => {
         const mobil = elementCreation.createMenuItem('Mobil 1 Extended Performance Full Synthetic Motor Oil 5W-30', '39.99', mobilImageFile, menuCategoryContainer.secondElement)
         const supertech = elementCreation.createMenuItem('Super Tech Full Synthetic SAE 5W-30 Motor Oil', '26.25', superTechImageFile, menuCategoryContainer.secondElement)
     })()
-
     const sidesMenu = (() => {
         const menuCategoryContainer = elementCreation.createContainer('menu-sub-container', 'menu-sub-title', 'Sides', 'menu-sub-content', '', tabContentContainer)
         const nutsBolts = elementCreation.createMenuItem('Assorted Nuts & Bolts', '12.00', nutsBoltsImageFile, menuCategoryContainer.secondElement)
         const spareTire = elementCreation.createMenuItem('Spare Tire Salad', '86.00', spareTireImageFile, menuCategoryContainer.secondElement)
+    })()
+    const entreeMenu = (() => {
+        const menuCategoryContainer = elementCreation.createContainer('menu-sub-container', 'menu-sub-title', 'Entrees', 'menu-sub-content', '', tabContentContainer)
+        const gasoline = elementCreation.createMenuItem('Midwest Gasoline', '32.98', gasolineImageFile, menuCategoryContainer.secondElement)
+        const scrapMetal = elementCreation.createMenuItem('Stainless Steel Shavings', '14.99', scrapMetalImageFile, menuCategoryContainer.secondElement)
+        const carBattery = elementCreation.createMenuItem('Duracell Car Battery Bisque', '214.99', carBatteryImageFile, menuCategoryContainer.secondElement)
+        const cables = elementCreation.createMenuItem('Copper Cable Pasta', '28.98', cablesImageFile, menuCategoryContainer.secondElement)
     })()
 }
 
